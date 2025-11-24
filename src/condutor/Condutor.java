601d9c922aae10ba71fc3e3dfe8ca66e8f73a76a
Gabriel Almeida <161779319+Almeiida04@users.noreply.github.com>
2025-11-24 19:06:24 -0300

    Update Condutor.java

--- src/condutor/Condutor.java
+++ src/condutor/Condutor.java
@@ -1,1 +1,40 @@
+package condutor;
+
+public class Condutor {
+
+    private String nomeCompleto;
+    private String numeroRegistro;
+    private int pontosAcumulados;
+
+    public Condutor(String nomeCompleto, String numeroRegistro) {
+        this.nomeCompleto = nomeCompleto;
+        this.numeroRegistro = numeroRegistro;
+        this.pontosAcumulados = 0;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getNumeroRegistro() {
+        return numeroRegistro;
+    }
+
+    public int getPontosAcumulados() {
+        return pontosAcumulados;
+    }
+
+    public void somarPontos(int pontos) {
+        this.pontosAcumulados += pontos;
+    }
+
+    @Override
+    public String toString() {
+        return "Condutor{" +
+                "nomeCompleto='" + nomeCompleto + '\'' +
+                ", numeroRegistro='" + numeroRegistro + '\'' +
+                ", pontosAcumulados=" + pontosAcumulados +
+                '}';
+    }
+}
 
