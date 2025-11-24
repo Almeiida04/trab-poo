e82bf6ba8f8557471896c50247f7e1a9f4ea9c21
Gabriel Almeida <161779319+Almeiida04@users.noreply.github.com>
2025-11-24 19:09:28 -0300

    Update CalculadoraMultaPorTipo.java

--- src/multa/CalculadoraMultaPorTipo.java
+++ src/multa/CalculadoraMultaPorTipo.java
@@ -1,1 +1,15 @@
+package multa;
+
+public class CalculadoraMultaPorTipo implements CalculadoraMulta {
+
+    @Override
+    public double calcularValor(TipoMulta tipoMulta) {
+        return tipoMulta.getValor();
+    }
+
+    @Override
+    public int calcularPontos(TipoMulta tipoMulta) {
+        return tipoMulta.getPontos();
+    }
+}
 
