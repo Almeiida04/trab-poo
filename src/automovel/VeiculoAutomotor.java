02a997b3713e2a121c8c8b1110cf438705a34be9
Gabriel Almeida <161779319+Almeiida04@users.noreply.github.com>
2025-11-24 19:08:12 -0300

    Update VeiculoAutomotor.java

--- src/automovel/VeiculoAutomotor.java
+++ src/automovel/VeiculoAutomotor.java
@@ -1,1 +1,42 @@
+package automovel;
+
+import condutor.Condutor;
+
+public class VeiculoAutomotor {
+
+    private String placa;
+    private String descricaoModelo;
+    private Condutor responsavel;
+
+    public VeiculoAutomotor(String placa, String descricaoModelo, Condutor responsavel) {
+        this.placa = placa;
+        this.descricaoModelo = descricaoModelo;
+        this.responsavel = responsavel;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getDescricaoModelo() {
+        return descricaoModelo;
+    }
+
+    public Condutor getResponsavel() {
+        return responsavel;
+    }
+
+    public void alterarResponsavel(Condutor novoResponsavel) {
+        this.responsavel = novoResponsavel;
+    }
+
+    @Override
+    public String toString() {
+        return "VeiculoAutomotor{" +
+                "placa='" + placa + '\'' +
+                ", descricaoModelo='" + descricaoModelo + '\'' +
+                ", responsavel=" + (responsavel != null ? responsavel.getNomeCompleto() : "N/A") +
+                '}';
+    }
+}
 
