8ef9823e125a70dc799a5757f25fcbed1d91a43c
Gabriel Almeida <161779319+Almeiida04@users.noreply.github.com>
2025-11-24 19:07:54 -0300

    Update AvaliadorSituacaoCarteiraPadrao.java

--- src/condutor/AvaliadorSituacaoCarteiraPadrao.java
+++ src/condutor/AvaliadorSituacaoCarteiraPadrao.java
@@ -1,1 +1,16 @@
+package condutor;
+
+public class AvaliadorSituacaoCarteiraPadrao implements AvaliadorSituacaoCarteira {
+
+    @Override
+    public SituacaoCarteira avaliarSituacao(int pontosAtuais) {
+        if (pontosAtuais < 10) {
+            return SituacaoCarteira.REGULAR;
+        } else if (pontosAtuais < 20) {
+            return SituacaoCarteira.EM_OBSERVACAO;
+        } else {
+            return SituacaoCarteira.SUSPENSA;
+        }
+    }
+}
 
