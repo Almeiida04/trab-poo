d2bf0f21b91f961674f11f6f2fe7e02cb30c144d
Gabriel Almeida <161779319+Almeiida04@users.noreply.github.com>
2025-11-24 19:08:37 -0300

    Update TipoMulta.java

--- src/multa/TipoMulta.java
+++ src/multa/TipoMulta.java
@@ -1,1 +1,25 @@
+package multa;
+
+public enum TipoMulta {
+    LEVE(3, 88.30),
+    MEDIA(4, 130.16),
+    GRAVE(5, 195.23),
+    GRAVISSIMA(7, 293.47);
+
+    private final int pontos;
+    private final double valor;
+
+    TipoMulta(int pontos, double valor) {
+        this.pontos = pontos;
+        this.valor = valor;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
 
